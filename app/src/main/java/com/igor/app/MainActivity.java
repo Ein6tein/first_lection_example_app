79c32f6046d0e044c9b4abf648f0975cd77604f9
igor.chernishenko <igor.chernishenko@ecentria.com>
2020-01-14 21:21:05 +0200

    Initial commit

---
+++ app/src/main/java/com/igor/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.igor.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.igor.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
